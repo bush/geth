dc9993765185ecff9a8f0c96b621daebd9343b43
Dave Bush <davebush@gmail.com>
2018-04-09 10:44:48 +0000

    Fixed volume mount for geth-testnet-console.sh
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- geth-testnet-console.sh
+++ geth-testnet-console.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v /home/dave/geth/ethereum:/root ethereum/client-go attach /root/.ethereum/testnet/geth.ipc 
+docker run --rm -it -v /home/dave/geth:/root ethereum/client-go attach /root/.ethereum/testnet/geth.ipc 
