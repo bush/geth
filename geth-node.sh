64577cd67732801c1aff2ff1534a065c6ca7d0c9
Dave Bush <davebush@gmail.com>
2018-04-12 01:07:14 +0000

    Minor config change.
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- geth-node.sh
+++ geth-node.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm --name geth-node -v /home/dave/geth:/root -p 127.0.0.1:8545:8545 -p 30303:30303 ethereum/client-go --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,web3,net --rpccorsdomain "*" --syncmode "full" --cache 2048 
+docker run -d --name geth-node --restart always -v /home/dave/geth:/root -p 127.0.0.1:8545:8545 -p 30303:30303 ethereum/client-go --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,web3,net --rpccorsdomain "*" --syncmode "full" --cache 1024 
